69e9059142dea90166da2858d68bec1fa8601cfa
Tom Dong <contact@tomdong.io>
2020-08-18 23:40:12 -0500

    Added profile screen and signup screen navigation

--- src/screens/ProfileView/index.tsx
+++ src/screens/ProfileView/index.tsx
@@ -1,13 +1,41 @@
+import { useNavigation } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import React from "react";
-import { Text } from "react-native";
+import { Button, Text } from "react-native";
 
 import { Layout } from "../../components/Layout";
+import { SignUpScreen } from "../SignUpScreen";
 
-const ProfileView: React.FC = (): JSX.Element => {
+const ProfileStack = createStackNavigator();
+
+const ProfileViewDefault: React.FC = (): JSX.Element => {
+  const navigation = useNavigation();
+
   return (
     <Layout>
       <Text>Profile Screen</Text>
+      <Button
+        title="Sign In"
+        onPress={() => navigation.navigate("ProfileLogin")}
+      />
+      <Button
+        title="Sign Up"
+        onPress={() => navigation.navigate("ProfileSignUp")}
+      />
     </Layout>
+  );
+};
+
+const ProfileView: React.FC = (): JSX.Element => {
+  return (
+    <ProfileStack.Navigator>
+      <ProfileStack.Screen
+        name="ProfileScreen"
+        component={ProfileViewDefault}
+      />
+      <ProfileStack.Screen name="ProfileLogin" component={SignUpScreen} />
+      <ProfileStack.Screen name="ProfileSignUp" component={SignUpScreen} />
+    </ProfileStack.Navigator>
   );
 };
 
---
+++ src/screens/SignUpScreen/index.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import { Text, TextInput, View } from "react-native";
+
+// TODO: Set up form validation
+// TODO: Connect to API
+// TODO: Needs styling
+const SignUpScreen: React.FC = (): JSX.Element => {
+  const [firstName, setFirstName] = useState("First Name");
+  const [lastName, setLastName] = useState("Last Name");
+  const [email, setEmail] = useState("email@example.com");
+  const [username, setUsername] = useState("Username");
+  const [password, setPassword] = useState("");
+
+  return (
+    <View>
+      <Text>First Name</Text>
+      <TextInput
+        value={firstName}
+        onChangeText={(value) => setFirstName(value)}
+      />
+      <Text>Last Name</Text>
+      <TextInput
+        value={lastName}
+        onChangeText={(value) => setLastName(value)}
+      />
+      <Text>Email</Text>
+      <TextInput value={email} onChangeText={(value) => setEmail(value)} />
+      <Text>Username</Text>
+      <TextInput
+        value={username}
+        onChangeText={(value) => setUsername(value)}
+      />
+      <Text>Password</Text>
+      <TextInput
+        value={password}
+        onChangeText={(value) => setPassword(value)}
+      />
+    </View>
+  );
+};
+
+export { SignUpScreen };
